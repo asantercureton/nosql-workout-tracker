98e6ff40875573468fc5a5f4e7198c6cf36f485a
Asante Cureton <asantercureton@gmail.com>
2021-10-13 19:24:49 -0400

    Checked all js and css links within exercise.html, index.html and stats.html, adding Workout.js file, and updated server.js with mongoose connection, required logger('morgan') and logger(dev) middleware

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
 const express = require('express').config();
-const mongojs = require("mongojs");
+const mongoose = require('mongoose');
 const routes = require('');
+
+const logger = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
+app.use(logger('dev'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useCreateIndex: true,
+});
+
 app.use(routes);
 
 app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`));
